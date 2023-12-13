628559d602430c40ba7c786fc35c5bf27b392dcf
harvey ian <112628848+hidimalibot@users.noreply.github.com>
2023-12-13 14:32:50 +0800

    s
    s

---
+++ models/userModel.js
@@ -0,0 +1,65 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    isDoctor: {
+      type: Boolean,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+    seenNotifications: {
+      type: Array,
+      default: [],
+    },
+    unseenNotifications: {
+      type: Array,
+      default: [],
+    },
+    age: {
+      type: Number,
+    },
+    gender: {
+      type: String,
+    },
+    address: {
+      type: String,
+    },
+    bloodType: {
+      type: String,
+    },
+    weight: {
+      type: Number,
+    },
+    height: {
+      type: Number,
+    },
+    birthday: {
+      type: Date,
+    },
+    phoneNumber: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const userModel = mongoose.model("users", userSchema);
+
+module.exports = userModel;
